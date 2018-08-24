88c2b03f0c74cac9ef78f54ad60fd0591e222d81
Kang SangHun <myks790@gmail.com>
2018-08-24 18:16:52 +0900

    카카오 코드 페스티벌 예선 1번(수정)

--- src/main/java/y2018/m8/d24_kakao_prize_hunter/Solution.py
+++ src/main/java/y2018/m8/d24_kakao_prize_hunter/Solution.py
@@ -1,15 +1,15 @@
-reward_2017 = [{"rank": 1, "reward": 5000000, "limit": 1},
+reward_2017 = ({"rank": 1, "reward": 5000000, "limit": 1},
                {"rank": 2, "reward": 3000000, "limit": 2},
                {"rank": 3, "reward": 2000000, "limit": 3},
                {"rank": 4, "reward": 500000, "limit": 4},
                {"rank": 5, "reward": 300000, "limit": 5},
-               {"rank": 6, "reward": 100000, "limit": 6}]
+               {"rank": 6, "reward": 100000, "limit": 6})
 
-reward_2018 = [{"rank": 1, "reward": 5120000, "limit": 1},
+reward_2018 = ({"rank": 1, "reward": 5120000, "limit": 1},
                {"rank": 2, "reward": 2560000, "limit": 2},
                {"rank": 3, "reward": 1280000, "limit": 4},
                {"rank": 4, "reward": 640000, "limit": 8},
-               {"rank": 5, "reward": 320000, "limit": 16}]
+               {"rank": 5, "reward": 320000, "limit": 16})
 
 
 def get_reward(reward_def, rank):
@@ -26,10 +26,10 @@ def get_reward(reward_def, rank):
 
 
 def solution():
-    cnt = input()
-    for i in range(int(cnt)):
-        rank_2017, rank_2018 = input().split(" ")
-        reward = get_reward(reward_2017, int(rank_2017)) + get_reward(reward_2018, int(rank_2018))
+    cnt = int(input())
+    for i in range(cnt):
+        rank_2017, rank_2018 = map(int, input().split(" "))
+        reward = get_reward(reward_2017, rank_2017) + get_reward(reward_2018, rank_2018)
         print(reward)
 
 
